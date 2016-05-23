9dc09e8105a834600a41231191f6f9d7566c1389
JohannBillar <jwbillar@gmail.com>
2016-05-22 17:58:23 -0700

    Setup Gulp Compass for Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
-var gulp = require('gulp');
+var gulp   = require('gulp'),
+   gutil   = require('gulp-util'),
+   concat  = require('gulp-concat'),
+   compass = require('gulp-compass');
 
-gulp.task('default', function() {
-  console.log('Gulp is working!');
+var sassSources = ['components/sass-boilerplate/stylesheets/main.scss'];   
+
+gulp.task('log', function() {
+  gutil.log('Workflow is working!');
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass-boilerplate/stylesheets',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'));
 });
