8e6a2d2a4223dbe908cc382aea80a772f79912a8
JohannBillar <jwbillar@gmail.com>
2016-05-24 18:15:28 -0700

    Add simple plain JS Email validation

--- components/scripts/form_validation.js
+++ components/scripts/form_validation.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", function init() {
   'use strict';
 
-  console.log('The form loaded :)');
-});
+  function validEmail(input) {
+    var emailRegExp = /^[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+$/i;
+    if (input) {
+      return emailRegExp.test(input);
+    } else {
+      return false;
+    }
+  }
+
+  function success(element) {
+    element.setAttribute('class', 'valid');
+  }
+  function failure(element) {
+    element.setAttribute('class', 'error');
+  }
+
+  var form = document.getElementsByTagName('form')[0]; 
+  form.addEventListener('keyup', function() {
+    
+    var emailInput      = document.getElementById('email');
+    var emailInputValue = emailInput.value;
+
+    if (validEmail(emailInputValue)) {
+      success(emailInput);
+    } else {
+      failure(emailInput);
+    }
+    
+  },false);
+
+},false);
